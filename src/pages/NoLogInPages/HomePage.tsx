645f6d0dd8e3ecfb1e87926737eaf7d5a6bd54fe
Albin Lindeborg <albin.lindeborg@gmail.com>
2023-09-18 11:19:18 +0200

    add navigation

--- src/pages/NoLogInPages/HomePage.tsx
+++ src/pages/NoLogInPages/HomePage.tsx
@@ -1,18 +1,16 @@
-import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button'
 import { Link } from 'react-router-dom'
 
 const HomePage = () => {
 	return (
-		<Container>
+		<>
 			<h1>Welcome to The Hangry App</h1>
 			<p>- Always by your side in case you get hangry 🔥🌭🍟🍔🍕🌮🥪🥙🥗</p>
 
 			<Link to='/map'>
 				<Button>See map</Button>
 			</Link>
-		</Container>
-
+		</>
 	)
 }
 
