3811a0e120ac4991ed8efd685cd686040134298d
Albin Lindeborg <albin.lindeborg@gmail.com>
2023-09-20 11:42:46 +0200

    add logout page

--- src/pages/NoLogInPages/LogoutPage.tsx
+++ src/pages/NoLogInPages/LogoutPage.tsx
@@ -1,8 +1,39 @@
+import useAuth from '../../hooks/useAuth'
+import { useEffect } from 'react'
+import Card from 'react-bootstrap/Card'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
 
 const LogoutPage = () => {
-    return (
-        <div>LogoutPage</div>
-    )
+	const navigate = useNavigate()
+	const { logout } = useAuth()
+
+	useEffect(() => {
+		const logoutUser = async () => {
+			await logout()
+			navigate('/login')
+			toast.success("Successfully logged out")
+		}
+		logoutUser()
+	}, [logout, navigate])
+
+	return (
+		<Container className='py-3 center-y'>
+			<Row>
+				<Col md={{ span: 6, offset: 3 }}>
+					<Card>
+						<Card.Body>
+							<Card.Title className='mb-3'>Log out</Card.Title>
+							<Card.Text>Please wait while you're being logged out...</Card.Text>
+						</Card.Body>
+					</Card>
+				</Col>
+			</Row>
+		</Container>
+	)
 }
 
 export default LogoutPage
