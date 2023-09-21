cfc43826a6be3787ab13467d53a3ccd048e61004
andrenormanlang <andrenormanlang@gmail.com>
2023-09-21 08:34:26 +0200

    fixed indentation and email repetition in restaurant form

--- src/pages/AdminPages/RestaurantFormPage.tsx
+++ src/pages/AdminPages/RestaurantFormPage.tsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import {
-  Alert,
-  Button,
-  Card,
-  Col,
-  Container,
-  Form,
-  Row,
+	Alert,
+	Button,
+	Card,
+	Col,
+	Container,
+	Form,
+	Row,
 } from "react-bootstrap";
 import PlacesAutoComplete from "../../components/NoLogInPages/MapPage.tsx/PlacesAutoComplete";
 import { Restaurant } from "../../types/Restaurant.types";
@@ -19,131 +19,140 @@ import { restaurantsCol } from "../../services/firebase";
 const libraries: Libraries = ["places"];
 
 const RestaurantFormPage = () => {
-	const [selectedPlace, setSelectedPlace] = useState<google.maps.LatLngLiteral | null>(null);
+	const [selectedPlace, setSelectedPlace] =
+		useState<google.maps.LatLngLiteral | null>(null);
 	const [isError, setIsError] = useState(false);
 	const [errorMessage, setErrorMessage] = useState<string | null>(null);
 	const [isLoading, setIsLoading] = useState(false);
 
-  const {
-    handleSubmit,
-    register,
-    setValue,
-    formState: { errors },
-  } = useForm<Restaurant>();
+	const {
+		handleSubmit,
+		register,
+		setValue,
+		formState: { errors },
+	} = useForm<Restaurant>();
 
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: import.meta.env.VITE_GEOCODE_API_KEY,
-    libraries: libraries,
-  });
+	const { isLoaded } = useLoadScript({
+		googleMapsApiKey: import.meta.env.VITE_GEOCODE_API_KEY,
+		libraries: libraries,
+	});
 
-  if (!isLoaded)
-    return (
-      <Container>
-        {/* add other loading animation here ltr */}
-        <p>Loading...</p>
-      </Container>
-    );
+	if (!isLoaded)
+		return (
+			<Container>
+				{/* add other loading animation here ltr */}
+				<p>Loading...</p>
+			</Container>
+		);
 
-  const onSubmit = async (data: Restaurant) => {
-    try {
-      setIsError(false);
-      setErrorMessage(null);
-      setIsLoading(true);
+	const onSubmit = async (data: Restaurant) => {
+		try {
+			setIsError(false);
+			setErrorMessage(null);
+			setIsLoading(true);
 
-      // Check if a location has been selected
-      if (!selectedPlace) {
-        setIsError(true);
-        setErrorMessage("Please select a location.");
-        setIsLoading(false);
-        return;
-      }
+		// Check if a location has been selected
+		if (!selectedPlace) {
+			setIsError(true);
+			setErrorMessage("Please select a location.");
+			setIsLoading(false);
+			return;
+		}
 
-			// console.log(selectedPlace);
+		// console.log(selectedPlace);
 
-			const newRestaurant: Restaurant = {
-				_id: data._id,
-				name: data.name,
-				streetAddress: data.streetAddress || "", // Provide a default value if not available
-				city: data.city ?? "", // Provide a default value if not available
-				description: data.description,
-				category: data.category,
-				supply: data.supply,
-				email: data.email || "", // Provide a default value if not available
-				telephone: data.telephone || "", // Provide a default value if not available
-				website: data.website || "", // Provide a default value if not available
-				facebook: data.facebook || "", // Provide a default value if not available
-				instagram: data.instagram || "", // Provide a default value if not available
-				location: selectedPlace,
-			};
-			const docRef = doc(restaurantsCol);
-			//   const restaurantsCol = collection(db, 'restaurants');
-			await setDoc(docRef, newRestaurant);
+		const newRestaurant: Restaurant = {
+			_id: data._id,
+			name: data.name,
+			streetAddress: data.streetAddress || "", // Provide a default value if not available
+			city: data.city ?? "", // Provide a default value if not available
+			description: data.description,
+			category: data.category,
+			supply: data.supply,
+			email: data.email || "", // Provide a default value if not available
+			telephone: data.telephone || "", // Provide a default value if not available
+			website: data.website || "", // Provide a default value if not available
+			facebook: data.facebook || "", // Provide a default value if not available
+			instagram: data.instagram || "", // Provide a default value if not available
+			location: selectedPlace,
+		};
+		const docRef = doc(restaurantsCol);
+		//   const restaurantsCol = collection(db, 'restaurants');
+		await setDoc(docRef, newRestaurant);
 
-			console.log(newRestaurant);
+		console.log(newRestaurant);
 
-      console.log("Restaurant added successfully!");
-      setIsLoading(false);
-    } catch (error) {
-      console.error("Error adding restaurant:", error);
+		console.log("Restaurant added successfully!");
+		setIsLoading(false);
+		} catch (error) {
+			console.error("Error adding restaurant:", error);
 
-      setIsError(true);
-      setErrorMessage("An error occurred while adding the restaurant.");
-      setIsLoading(false);
-    }
-  };
+			setIsError(true);
+			setErrorMessage("An error occurred while adding the restaurant.");
+			setIsLoading(false);
+		}
+	};
 
-  return (
-    <Container className="py-3 center-y">
-      <Row>
-        <Col md={{ span: 6, offset: 3 }}>
-          <Card>
-            <Card.Body>
-              <Card.Title className="mb-3">Add Restaurant</Card.Title>
-              {isError && <Alert variant="danger">{errorMessage}</Alert>}
+	return (
+		<Container className="py-3 center-y">
+			<Row>
+				<Col md={{ span: 6, offset: 3 }}>
+					<Card>
+						<Card.Body>
+							<Card.Title className="mb-3">
+								Add Restaurant
+							</Card.Title>
+							{isError && (
+								<Alert variant="danger">{errorMessage}</Alert>
+							)}
 
 							{/* Display the selected place data here */}
 							<div className="mb-3">
-								<label htmlFor="name" className="mb-2">Place</label>
+								<label htmlFor="name" className="mb-2">
+									Place
+								</label>
 								<PlacesAutoComplete
 									onClickedPlace={(results) => {
-										const selectedAddress =
-											results[0]?.formatted_address || "";
-
+										const selectedAddress =results[0]?.formatted_address || "";
 										// Set name as the selected address
 										setValue("name", selectedAddress);
-
 										// Set address as the selected address
-										setValue("streetAddress", selectedAddress);
+										setValue("streetAddress",selectedAddress);
 
 										// Set id to googles id on the place
 										setValue("_id", results[0]?.place_id);
 
 										// get and set the lat and long positions
-										const { lat, lng } = getLatLng(results[0])
-										setSelectedPlace({ lat, lng })
+										const { lat, lng } = getLatLng(results[0]);
+										setSelectedPlace({ lat, lng });
 
 										// Find the city component in the address_components array
-										results[0]?.address_components.find((component) => {
-											if (component.types[0] !== ("postal_town")) return
-											setValue("city", component.long_name)
-										})
+										results[0]?.address_components.find(
+											(component) => {
+												if (component.types[0] !=="postal_town")
+													return;
+												setValue("city",component.long_name);
+											}
+										);
 									}}
 								/>
 							</div>
 
-              <Form onSubmit={handleSubmit(onSubmit)}>
-                {/* Description */}
-                <Form.Group controlId="description" className="mb-3">
-                  <Form.Label>Description</Form.Label>
-                  <Form.Control
-                    as="textarea"
-                    rows={3}
-                    {...register("description")}
-                  />
-                </Form.Group>
+							<Form onSubmit={handleSubmit(onSubmit)}>
+								{/* Description */}
+								<Form.Group
+									controlId="description"
+									className="mb-3"
+								>
+									<Form.Label>Description</Form.Label>
+									<Form.Control
+										as="textarea"
+										rows={3}
+										{...register("description")}
+									/>
+								</Form.Group>
 
 								{/* Add more fields based on your Restaurant type */}
-
 
 								<div className="mb-3 d-flex align-items-center">
 									<label className="m-2" htmlFor="category">
@@ -151,15 +160,23 @@ const RestaurantFormPage = () => {
 									</label>
 									<select
 										id="category"
-										{...register('category')}
+										{...register("category")}
 										className="form-select"
-										style={{ flex: 0.7, maxWidth: '150px' }} // Adjust flex and maxWidth as needed
+										style={{ flex: 0.7, maxWidth: "150px" }} // Adjust flex and maxWidth as needed
 									>
 										<option value="Café">Café</option>
-										<option value="Restaurant">Restaurant</option>
-										<option value="Fast food">Fast food</option>
-										<option value="Kiosk/grill">Kiosk/grill</option>
-										<option value="Food truck">Food truck</option>
+										<option value="Restaurant">
+											Restaurant
+										</option>
+										<option value="Fast food">
+											Fast food
+										</option>
+										<option value="Kiosk/grill">
+											Kiosk/grill
+										</option>
+										<option value="Food truck">
+											Food truck
+										</option>
 									</select>
 								</div>
 
@@ -169,15 +186,40 @@ const RestaurantFormPage = () => {
 									</label>
 									<select
 										id="supply"
-										{...register('supply')}
+										{...register("supply")}
 										className="form-select"
-										style={{ flex: 0.7, maxWidth: '150px' }} // Adjust flex and maxWidth as needed
+										style={{ flex: 0.7, maxWidth: "150px" }} // Adjust flex and maxWidth as needed
 									>
 										<option value="Lunch">Lunch</option>
-										<option value="After Work">After Work</option>
-										<option value="Middag/Á la carte">Middag/Á la carte</option>
+										<option value="After Work">
+											After Work
+										</option>
+										<option value="Middag/Á la carte">
+											Middag/Á la carte
+										</option>
 									</select>
 								</div>
+								{/* Supply */}
+								<div className="mb-3 d-flex align-items-center">
+									<label className="m-2" htmlFor="supply">
+										Supply
+									</label>
+									<select
+										id="supply"
+										{...register("supply")}
+										className="form-select"
+										style={{ flex: 0.7, maxWidth: "150px" }}
+									>
+										<option value="Lunch">Lunch</option>
+										<option value="After Work">
+											After Work
+										</option>
+										<option value="Middag/Á la carte">
+											Middag/Á la carte
+										</option>
+									</select>
+								</div>
+
 								{/* E-mail */}
 								<Form.Group controlId="email" className="mb-3">
 									<Form.Label>E-mail</Form.Label>
@@ -185,37 +227,18 @@ const RestaurantFormPage = () => {
 										type="email"
 										{...register("email")}
 									/>
-								</Form.Group>
-								{/* Supply */}
-								<div className="mb-3 d-flex align-items-center">
-								<label className="m-2" htmlFor="supply">
-									Supply
-								</label>
-								<select
-									id="supply"
-									{...register("supply")}
-									className="form-select"
-									style={{ flex: 0.7, maxWidth: "150px" }}
-								>
-									<option value="Lunch">Lunch</option>
-									<option value="After Work">After Work</option>
-									<option value="Middag/Á la carte">Middag/Á la carte</option>
-								</select>
-								</div>
-
-								{/* E-mail */}
-								<Form.Group controlId="email" className="mb-3">
-								<Form.Label>E-mail</Form.Label>
-								<Form.Control type="email" {...register("email")} />
-								{errors.email && (
-									<Form.Text className="text-danger">
-									{errors.email.message}
-									</Form.Text>
-								)}
+									{errors.email && (
+										<Form.Text className="text-danger">
+											{errors.email.message}
+										</Form.Text>
+									)}
 								</Form.Group>
 
 								{/* Telephone */}
-								<Form.Group controlId="telephone" className="mb-3">
+								<Form.Group
+									controlId="telephone"
+									className="mb-3"
+								>
 									<Form.Label>Telephone</Form.Label>
 									<Form.Control
 										type="tel"
@@ -223,15 +246,23 @@ const RestaurantFormPage = () => {
 									/>
 								</Form.Group>
 
-
 								{/* Website */}
-								<Form.Group controlId="website" className="mb-3">
-								<Form.Label>Website</Form.Label>
-								<Form.Control type="url" {...register("website")} />
+								<Form.Group
+									controlId="website"
+									className="mb-3"
+								>
+									<Form.Label>Website</Form.Label>
+									<Form.Control
+										type="url"
+										{...register("website")}
+									/>
 								</Form.Group>
 
 								{/* Facebook */}
-								<Form.Group controlId="facebook" className="mb-3">
+								<Form.Group
+									controlId="facebook"
+									className="mb-3"
+								>
 									<Form.Label>Facebook</Form.Label>
 									<Form.Control
 										type="text"
@@ -240,7 +271,10 @@ const RestaurantFormPage = () => {
 								</Form.Group>
 
 								{/* Instagram */}
-								<Form.Group controlId="instagram" className="mb-3">
+								<Form.Group
+									controlId="instagram"
+									className="mb-3"
+								>
 									<Form.Label>Instagram</Form.Label>
 									<Form.Control
 										type="text"
@@ -253,7 +287,9 @@ const RestaurantFormPage = () => {
 									type="submit"
 									className="mt-3"
 								>
-									{isLoading ? "Adding Restaurant..." : "Add Restaurant"}
+									{isLoading
+										? "Adding Restaurant..."
+										: "Add Restaurant"}
 								</Button>
 							</Form>
 						</Card.Body>
@@ -265,5 +301,3 @@ const RestaurantFormPage = () => {
 };
 
 export default RestaurantFormPage;
-
-
