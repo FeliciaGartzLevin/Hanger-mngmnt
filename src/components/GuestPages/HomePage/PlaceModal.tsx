7ea5085817e53621523d60698d29c2028e0025a8
Albin Lindeborg <albin.lindeborg@gmail.com>
2023-09-25 10:43:07 +0200

    put Cols inside conditional

--- src/components/GuestPages/HomePage/PlaceModal.tsx
+++ src/components/GuestPages/HomePage/PlaceModal.tsx
@@ -32,41 +32,45 @@ const PlaceModal: React.FC<IProps> = ({ onClose, place, show }) => {
 					className='mt-3 justify-content-end'
 					xs='auto'
 				>
-					<Col>
-						{place.website && (
+					{place.website && (
+						<Col>
 							<Link to={place.website} target='_blank'>
 								<FontAwesomeIcon icon={faGlobe} size='xl' />
 							</Link>
-						)}
-					</Col>
-					<Col>
-						{place.facebook && (
+						</Col>
+					)}
+
+					{place.facebook && (
+						<Col>
 							<Link to={place.facebook} target='_blank'>
 								<FontAwesomeIcon icon={faSquareFacebook} size='xl' />
 							</Link>
-						)}
-					</Col>
-					<Col>
-						{place.instagram && (
+						</Col>
+					)}
+
+					{place.instagram && (
+						<Col>
 							<Link to={place.instagram} target='_blank'>
 								<FontAwesomeIcon icon={faSquareInstagram} size='xl' />
 							</Link>
-						)}
-					</Col>
-					<Col>
-						{place.telephone && (
+						</Col>
+					)}
+
+					{place.telephone && (
+						<Col>
 							<Link to={`tel:${place.telephone}`} target='_blank'>
 								<FontAwesomeIcon icon={faPhone} size='xl' />
 							</Link>
-						)}
-					</Col>
-					<Col>
-						{place.email && (
+						</Col>
+					)}
+
+					{place.email && (
+						<Col>
 							<Link to={`mailto:${place.email}`}>
 								<FontAwesomeIcon icon={faEnvelope} size='xl' />
 							</Link>
-						)}
-					</Col>
+						</Col>
+					)}
 				</Row>
 			</Modal.Body>
 			<Modal.Footer>
