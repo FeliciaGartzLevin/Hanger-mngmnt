24ec635cb8712775e33b65bbac2889c8599de561
andrenormanlang <andrenormanlang@gmail.com>
2023-09-26 19:52:05 +0200

    added react bootstrap pagination

--- src/components/AdminPages/AdminPlacesSortableTable.tsx
+++ src/components/AdminPages/AdminPlacesSortableTable.tsx
@@ -1,8 +1,8 @@
-import { doc, updateDoc } from "firebase/firestore";
-import { placesCol } from "../../services/firebase";
-import { useState } from "react";
-import Form from "react-bootstrap/Form";
-import Table from "react-bootstrap/Table";
+import { doc, updateDoc } from 'firebase/firestore'
+import { placesCol } from '../../services/firebase'
+import { useState } from 'react'
+import Form from 'react-bootstrap/Form'
+import Table from 'react-bootstrap/Table'
 import {
 	ColumnDef,
 	flexRender,
@@ -10,137 +10,117 @@ import {
 	getSortedRowModel,
 	SortingState,
 	useReactTable,
-} from "@tanstack/react-table";
-import { Place } from "../../types/Place.types";
-import Pagination  from '../../components/Pagination'
+} from '@tanstack/react-table'
+import { Place } from '../../types/Place.types'
 
 interface IProps<TData, TValue> {
-	columns: ColumnDef<TData, TValue>[];
-	data: TData[];
+	columns: ColumnDef<TData, TValue>[]
+	data: TData[]
 }
 
 const AdminUsersSortableTable = <TData, TValue>({
 	columns,
 	data,
 }: IProps<TData, TValue>) => {
-	const [currentPage, setCurrentPage] = useState(1);
-	const [itemsPerPage, setItemsPerPage] = useState(10);
-	const [sorting, setSorting] = useState<SortingState>([]);
-
-	const indexOfLastItem = currentPage * itemsPerPage;
-	const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-	const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+	const [sorting, setSorting] = useState<SortingState>([])
 
 	const table = useReactTable({
 		columns,
-		data: currentItems,
+		data,
 		state: {
 			sorting,
 		},
 		onSortingChange: setSorting,
 		getCoreRowModel: getCoreRowModel(),
-		getSortedRowModel: getSortedRowModel(),
-	});
+		getSortedRowModel: getSortedRowModel()
+	})
 
 	const toggleApproval = (PlaceData: Place) => {
-		const docRef = doc(placesCol, PlaceData._id);
+		const docRef = doc(placesCol, PlaceData._id)
 		updateDoc(docRef, {
-			isApproved: !PlaceData.isApproved,
-		});
-	};
+			isApproved: !PlaceData.isApproved
+		})
+	}
 
 	const renderApprovalCell = (PlaceData: Place) => (
 		<Form>
 			<Form.Check
 				checked={PlaceData.isApproved}
-				id="approval-switch"
+				id='approval-switch'
 				onChange={() => toggleApproval(PlaceData)}
-				type="switch"
+				type='switch'
 			/>
 		</Form>
-	);
+	)
 
 	const cellRenderer = (cellType: string, PlaceData: Place) => {
 		switch (cellType) {
-			case "isApproved":
-				return renderApprovalCell(PlaceData);
+			case 'isApproved':
+				return renderApprovalCell(PlaceData)
 			default:
-				return;
+				return
 		}
-	};
+	}
 
-	const altRendering = ["isApproved"];
+	const altRendering = [ 'isApproved']
 
 	return (
-		<>
-			<Table striped bordered hover responsive>
-				<thead>
-					{table.getHeaderGroups().map((headerGroup) => (
-						<tr key={headerGroup.id}>
-							{headerGroup.headers.map((header) => (
-								<th key={header.id} colSpan={header.colSpan}>
-									{header.isPlaceholder ? null : (
-										<div
-											className={
+		<Table striped bordered hover responsive>
+			<thead>
+				{table.getHeaderGroups().map((headerGroup) => (
+					<tr key={headerGroup.id}>
+						{headerGroup.headers.map((header) => (
+							<th key={header.id} colSpan={header.colSpan}>
+								{header.isPlaceholder ? null : (
+									<div
+										{...{
+											className:
 												header.column.getCanSort()
-													? "cursor-pointer select-none"
-													: ""
-											}
-											onClick={header.column.getToggleSortingHandler()}
-										>
-											{flexRender(
-												header.column.columnDef.header,
-												header.getContext()
-											)}
-											{{
-												asc: " ⬆",
-												desc: " ⬇",
-											}[
-												header.column.getIsSorted() as string
-											] ?? null}
-										</div>
-									)}
-								</th>
-							))}
-						</tr>
-					))}
-				</thead>
+													? 'cursor-pointer select-none'
+													: '',
+											onClick:
+												header.column.getToggleSortingHandler()
+										}}
+									>
+										{flexRender(
+											header.column.columnDef.header,
+											header.getContext()
+										)}
 
-				<tbody>
-					{table.getRowModel().rows.map((row) => (
-						<tr key={row.id}>
-							{row
-								.getVisibleCells()
-								.map((cell) =>
-									cell.column.id in altRendering ? (
-										<td key={cell.id}>
-											{cellRenderer(
-												cell.column.id,
-												row.original as Place
-											)}
-										</td>
-									) : (
-										<td key={cell.id}>
-											{flexRender(
-												cell.column.columnDef.cell,
-												cell.getContext()
-											)}
-										</td>
+										{{
+											asc: " ⬆",
+											desc: " ⬇"
+										}[
+											header.column.getIsSorted() as string
+										] ?? null}
+									</div>
+								)}
+							</th>
+						))}
+					</tr>
+				))}
+			</thead>
+
+			<tbody>
+				{table.getRowModel().rows.map((row) => (
+					<tr key={row.id}>
+						{row.getVisibleCells().map((cell) => (
+							<td key={cell.id}>
+								{altRendering.includes(cell.column.id)
+								? cellRenderer(cell.column.id, row.original as Place)
+								: (
+									flexRender(
+										cell.column.columnDef.cell,
+										cell.getContext()
 									)
 								)}
-						</tr>
-					))}
-				</tbody>
-			</Table>
+							</td>
+						))}
+					</tr>
+				))}
+			</tbody>
+		</Table>
+	)
+}
 
-			<Pagination
-				currentPage={currentPage}
-				setCurrentPage={setCurrentPage}
-				totalItems={data.length}
-				itemsPerPage={itemsPerPage}
-			/>
-		</>
-	);
-};
-
-export default AdminUsersSortableTable;
+export default AdminUsersSortableTable
