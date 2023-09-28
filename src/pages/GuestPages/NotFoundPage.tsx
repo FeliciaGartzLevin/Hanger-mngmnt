386fb06bb12bbcdd949bc42c1e7de72a2896230a
Albin Lindeborg <albin.lindeborg@gmail.com>
2023-09-28 16:06:35 +0200

    fix styling

--- src/pages/GuestPages/NotFoundPage.tsx
+++ src/pages/GuestPages/NotFoundPage.tsx
@@ -4,7 +4,7 @@ import image from '../../assets/images/fedup22-picard.jpg';
 const NotFoundPage = () => {
   return (
     <div className="text-center">
-      <h1 className='mb-4'>Sorry that page could not be found and....</h1>
+      <h1 className='my-4 title'>Sorry that page could not be found and....</h1>
 
       <div className="d-flex justify-content-center">
         <Image
