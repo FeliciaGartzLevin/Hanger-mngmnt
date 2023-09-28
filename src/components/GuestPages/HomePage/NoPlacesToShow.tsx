77af23b0dc3c7e4f1e935ce8aa4545ba63061ab7
FeliciaGartzLevin <felicia.gartz.levin@gmail.com>
2023-09-28 13:35:23 +0200

    created components for sidebar + offcanvas for easy overview

---
+++ src/components/GuestPages/HomePage/NoPlacesToShow.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Alert from 'react-bootstrap/Alert'
+import Button from 'react-bootstrap/Button'
+import { Place } from '../../../types/Place.types'
+import { Link } from 'react-router-dom'
+
+type Props = {
+	places: Place[]
+}
+
+const NoPlacesToShow: React.FC<Props> = ({ places }) => {
+	return places && places.length <= 0 &&
+		<Alert variant="warning">
+			No places to show.{' '}
+			<Link
+				to={'/place-form'}
+				className="">
+				<Button
+					className="reccomend-btn"
+					variant="warning"
+				>
+					Recommend us a place
+				</Button>
+			</Link>
+		</Alert>
+}
+
+
+export default NoPlacesToShow
